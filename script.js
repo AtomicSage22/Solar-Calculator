c3edcd5b411dc2f4c2a7d81ad298e5f3fe1b2793
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2023-01-02 22:07:14 +0530

    add functionality and layout style

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-let houseload = 350;
+let houseload = document.querySelector("#energy-consumption");
 let batteryDcVolt = 12;
 let backUpTime = 8;
-let chargingTime = batteryCalc(houseload, backUpTime, batteryDcVolt)/10;
-let solarCurrent = chargingTime + inverterCalc(houseload);  
-let solarPower = solarCurrent * 14;
 
-let optionOne = (power) => {
-    return Math.ceil(power/125);
-}
-
-let optionTwo = (power) => {
-    return Math.ceil(power/180);
-}
-
-
-let inverterCalc = (load) =>{
+function inverterCalc(load){
     return Math.ceil(load / batteryDcVolt);
 }
 
-let batteryCalc = (load, time, volt) =>{
+function batteryCalc(load, time, volt){
     return Math.ceil(((load * time) /volt)/50) * 50;
 }  
 
-console.log(inverterCalc(houseload));
-console.log(batteryCalc(houseload, backUpTime, batteryDcVolt)); 
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!houseload.value) return;
+    let chargingTime = batteryCalc(houseload.value, backUpTime, batteryDcVolt)/10;
+    let solarCurrent = chargingTime + inverterCalc(houseload.value);  
+    let solarPower = solarCurrent * 14;
+    document.querySelector(".answer").textContent = Math.ceil(solarPower/document.querySelector('input[name="power"]:checked').value);
+})
